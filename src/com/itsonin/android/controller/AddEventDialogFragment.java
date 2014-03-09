b986c652e0d5264ae29c8da8e1e11d038127a07e
John Arley Burns <johnarleyburns@gmail.com>
2014-03-09 19:50:19 +0100

    calendar library

---
+++ src/com/itsonin/android/controller/AddEventDialogFragment.java
@@ -0,0 +1,11 @@
+package com.itsonin.android.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: johnarleyburns
+ * Date: 3/9/14
+ * Time: 11:33 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AddEventDialogFragment {
+}
---
+++ src/com/itsonin/android/model/Host.java
@@ -0,0 +1,11 @@
+package com.itsonin.android.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: johnarleyburns
+ * Date: 3/9/14
+ * Time: 4:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Host {
+}
