352919763a4eef294de412a76521b57675024f70
John Arley Burns <johnarleyburns@gmail.com>
2014-03-20 20:28:01 +0100

    aded staggered grid for potential tablet

---
+++ src/com/itsonin/android/api/ItsoninAPI.java
@@ -0,0 +1,11 @@
+package com.itsonin.android.api;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: johnarleyburns
+ * Date: 3/20/14
+ * Time: 5:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ItsoninAPI {
+}
---
+++ src/com/itsonin/android/model/Device.java
@@ -0,0 +1,51 @@
+package com.itsonin.android.model;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: johnarleyburns
+ * Date: 3/9/14
+ * Time: 4:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Device {
+
+    private static final String PREF_HOST_NAMES = "hostNames";
+    private static final String PREF_HOST_LAST_NAME = "hostLastName";
+
+    public Set<String> names = new HashSet<String>();
+    public String lastName;
+
+    public Device() {
+    }
+
+    public String[] rememberedNames() {
+        String[] namesArray = new String[names.size()];
+        return names.toArray(namesArray);
+    }
+
+
+    public void store(Context context) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putStringSet(PREF_HOST_NAMES, new HashSet<String>())
+                .putString(PREF_HOST_LAST_NAME, lastName)
+                .apply();
+    }
+
+    public static final Device load(Context context) {
+        Device host = new Device();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        Set<String> names = prefs.getStringSet(PREF_HOST_NAMES, new HashSet<String>());
+        host.names.addAll(names);
+        host.lastName =  prefs.getString(PREF_HOST_LAST_NAME, null);
+        return host;
+    }
+
+}
